e79fa7e6b3a827b210d4a2a427b559ed9434d0f3
Siugou <jefferycyjiao@gmail.com>
2026-04-12 20:01:23 +0800

    Removed extra useless comments
    I know what I wrote

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//IMPORTANT!! ZOOM LEVELS. Must change after creation of map. 
 const map = L.map('map', { 
     crs: L.CRS.Simple,
     minZoom: -2,
     maxZoom: 0,
     zoomSnap: 2,
-    zoomDelta: 2
+    zoomDelta: 2,
 });
 
-//Size of map created in px.
 const width = 2000;
 const height = 1200;
 
 const bounds = [[0, 0], [height, width]];
+
+map.setMaxBounds (bounds);
 
 const normalMap= L.imageOverlay('map.png', bounds);
 const detailedMap = L.imageOverlay('map_detailed.png', bounds);
@@ -19,20 +19,17 @@ const detailedMap = L.imageOverlay('map_detailed.png', bounds);
 normalMap.addTo(map);
 detailedMap.addTo(map);
 map.fitBounds(bounds);
-detailedMap.setOpacity(0); // Start with the detailed map hidden
+detailedMap.setOpacity(0);
 
-//icon!
 const myIcon = L.divIcon({
     className: 'custom-marker',
     html: '<div class="marker-dot"></div>',
     iconSize: [20, 20],
-    iconAnchor: [10, 10]
+    iconAnchor: [9, 10]
 });
 
-//making the maps change. 
 map.on('zoomend', function(){
     const zoom = map.getZoom();
-
     if (zoom === -2) {
         normalMap.setOpacity(1);
         detailedMap.setOpacity(0);
@@ -42,7 +39,7 @@ map.on('zoomend', function(){
     }
 });
 
-//Creating a set of information for the loop. EDIT ALL STUFF HERE
+//Edit basic descriptioons and locations here :)
 const locations = [
     {
     name: "City A",
@@ -61,34 +58,38 @@ const locations = [
     coords: [600, 900],
     description: "Montag escapes here and follows it to safety.",
     id: "river"
+    },
+    {
+    name: "Bookstore",
+    coords: [400, 700],
+    description: "A hidden place where Montag discovers forbidden knowledge.",
+    id: "bookstore"
     }
 ];
 
 locations.forEach(location => {
     const marker = L.marker(location.coords, {icon: myIcon}).addTo(map);
 
-    // Label (always visible)
     marker.bindTooltip(location.name, {
         permanent: true,
         direction: "top",
         className: "label"
      });
 
-    // Popup (description)
     marker.bindPopup(`
         <div class="popup-box">
             <h4>${location.name}</h4>
             <p>${location.description}</p>
             <a href="#${location.id}" class="more-link">Show more</a>
         </div>
-     `);
+     `,{
+        closeButton: false,
+     });
 
-     // Hide label when popup opens
-     marker.on('popupopen', () => {
+    marker.on('popupopen', () => {
         marker.closeTooltip();
      });
 
-    // Show label again when popup closes
     marker.on('popupclose', () => {
         marker.openTooltip();
     });
